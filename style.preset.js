786f4fd1e76826025f49074bb7f5ef1da7e5730a
aechelsea <apiwataeinth@gmail.com>
2022-07-02 12:32:10 +0700

    lastest

--- style.preset.js
+++ style.preset.js
@@ -9,6 +9,8 @@ module.exports = {
         darkorange: "#F6851B",
         blueshadow: "#4A63BC",
         bdpurple: "#545088",
+        tpurple: "#3400FF",
+        tpurples: "#5C39E5",
         },
 
         boxShadow: {
