0824dc0fd3934cd5f3a797eb234a817c2b5c94d6
aechelsea <apiwataeinth@gmail.com>
2022-07-07 20:50:14 +0700

    test

--- components/VoteForm.tsx
+++ components/VoteForm.tsx
@@ -55,28 +55,29 @@ const VoteForm = () => {
             </button>
           </div>
         </div>
-        <div className="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+          <span className="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">
+            First Slide
+          </span>
+          <img
+            src="/docs/images/carousel/carousel-1.svg"
+            className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+            alt="..."
+          />
+        </div>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="/docs/images/carousel/carousel-2.svg"
+            className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+            alt="..."
+          />
+        </div>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+          <img
+            src="/docs/images/carousel/carousel-3.svg"
+            className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+            alt="..."
+          />
         </div>
         <button
           type="button"
